2f11c88b4b264f85eae4b3840daff49cf1fefd80
Luqman Nurhakim <luqmanrosli907@gmail.com>
2024-11-27 10:00:54 +0800

    nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,21 +2,19 @@ import React from 'react';
 
 function Navbar() {
   return (
-    <header>
-      <nav className="bg-blue-600 text-white p-4 flex justify-between items-center">
-        <div className="flex items-center space-x-4">
+    <header className="bg-white text-gray-800">
+      <nav className='container mx-auto flex items-center justify-between py-4 px-6'>
+        <div className='flex items-center space-x-4 '>
           <img
             src={`${process.env.PUBLIC_URL}/images/img-home.jpg`}
-            alt="Banner"
-            className="h-12 w-12 rounded-full" // Adjust size and style as needed
-          />
-          <h1 className="text-2xl font-bold">ReactFacts</h1>
+            alt="Banner" className='w-14 h-14 rounded-full'/>
+          <h1 className='text-2xl font-bold'>ReactFacts</h1>
         </div>
-        <ul className="flex space-x-8 text-lg">
-          <li className="hover:underline cursor-pointer">Home</li>
-          <li className="hover:underline cursor-pointer">About</li>
-          <li className="hover:underline cursor-pointer">Services</li>
-          <li className="hover:underline cursor-pointer">Contact</li>
+        <ul className='flex space-x-6'>
+          <li> <a href='#home' className='hover:text-blue-500 transition duration-300'>Home </a> </li> 
+          <li>About</li>
+          <li>Services</li>
+          <li>Contact</li>
         </ul>
       </nav>
     </header>
