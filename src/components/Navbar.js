173d5c6550ae392c4ec374a9b1544791a34f9be2
Luqman Nurhakim <luqmanrosli907@gmail.com>
2024-12-18 16:50:30 +0800

    rr

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,52 @@
-import React, {useState} from 'react';
-//guna router unutk navigate ke page lain
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
   return (
     <header className="bg-black text-white">
-      <nav className='container mx-auto flex items-center justify-between py-3 px-6'>
-        <div className='flex items-center space-x-4 '>
+      <nav className="container mx-auto flex items-center justify-between py-3 px-6">
+        <div className="flex items-center space-x-4">
           <img
-            src={require (`../images/gambar-luqman.jpg`)}
-            alt="Banner" className='w-14 h-14 rounded-full'/>
-          <h1 className='text-2xl font-bold'>ReactFacts</h1>
+            src={require('../images/gambar-luqman.jpg')}
+            alt="Banner"
+            className="w-14 h-14 rounded-full"
+          />
+          <h1 className="text-2xl font-bold">ReactFacts</h1>
         </div>
-        <ul className='flex space-x-6 font-bold'>
+        <ul className="hidden md:flex space-x-6 font-bold">
           <li>
-              <Link to='/' className="hover:text-blue-500 transition duration-300" >Home</Link>
-          </li> 
-          <li>
-          <Link to='/about' className="hover:text-blue-500 transition duration-300" >About</Link>
+            <Link to="/" className="hover:text-blue-500 transition duration-300">
+              Home
+            </Link>
           </li>
           <li>
-          <Link to='/Services' className="hover:text-blue-500 transition duration-300" >Services</Link>
-            </li>
+            <Link to="/about" className="hover:text-blue-500 transition duration-300">
+              About
+            </Link>
+          </li>
           <li>
-          <Link to='/Contact' className="hover:text-blue-500 transition duration-300" >Contact</Link>
+            <Link to="/Services" className="hover:text-blue-500 transition duration-300">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link to="/Contact" className="hover:text-blue-500 transition duration-300">
+              Contact
+            </Link>
           </li>
         </ul>
 
-        {/*Hamburger Menu*/}
-        <button 
+        {/* Hamburger Menu */}
+        <button
           className="block md:hidden focus:outline-none"
-          onClick={toggleMenu}>
-        <svg
+          onClick={toggleMenu}
+        >
+          <svg
             className="w-6 h-6"
             fill="none"
             stroke="currentColor"
@@ -46,16 +57,15 @@ function Navbar() {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth={2}
-              d={isMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+              d={isMenuOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16M4 18h16'}
             />
           </svg>
         </button>
-                
       </nav>
 
       {/* Mobile Menu */}
       {isMenuOpen && (
-        <ul className="md:hidden flex flex-col items-center bg-black space-y-4 py-4 font-bold">
+        <ul className="md:hidden absolute top-0 left-0 w-full bg-black space-y-4 py-4 font-bold text-center">
           <li>
             <Link
               to="/"
