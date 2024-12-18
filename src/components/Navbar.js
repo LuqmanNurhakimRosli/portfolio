71e2e47e81190b06476aa173984fa2c8eb86630d
Luqman Nurhakim <luqmanrosli907@gmail.com>
2024-12-18 16:27:18 +0800

    update navbar + hamburger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 //guna router unutk navigate ke page lain
 import { Link } from 'react-router-dom'
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <header className="bg-black text-white">
       <nav className='container mx-auto flex items-center justify-between py-3 px-6'>
@@ -26,7 +30,70 @@ function Navbar() {
           <Link to='/Contact' className="hover:text-blue-500 transition duration-300" >Contact</Link>
           </li>
         </ul>
+
+        {/*Hamburger Menu*/}
+        <button 
+          className="block md:hidden focus:outline-none"
+          onClick={toggleMenu}>
+        <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d={isMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+            />
+          </svg>
+        </button>
+                
       </nav>
+
+      {/* Mobile Menu */}
+      {isMenuOpen && (
+        <ul className="md:hidden flex flex-col items-center bg-black space-y-4 py-4 font-bold">
+          <li>
+            <Link
+              to="/"
+              className="hover:text-blue-500 transition duration-300"
+              onClick={toggleMenu}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              className="hover:text-blue-500 transition duration-300"
+              onClick={toggleMenu}
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/Services"
+              className="hover:text-blue-500 transition duration-300"
+              onClick={toggleMenu}
+            >
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/Contact"
+              className="hover:text-blue-500 transition duration-300"
+              onClick={toggleMenu}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      )}
     </header>
   );
 }
