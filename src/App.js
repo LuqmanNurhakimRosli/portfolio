4dc9ae74a672ab11fe50525fc348d6d361b2a2c1
Luqman Nurhakim <luqmanrosli907@gmail.com>
2024-12-19 09:33:13 +0800

    add

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from './components/pages/Home.js';
 import About from './components/pages/About.js';
 import Services from './components/pages/Services.js';
 import Contact from './components/pages/Contact.js';
+import { Analytics } from "@vercel/analytics/react";
+
 
 
 export default function App () {
@@ -12,11 +14,13 @@ export default function App () {
   return (
     <div className='app'>
       <Routes>
+      <Route path='*' element={<h1>404 - Page Not Found</h1>} />
       <Route path='/' element={<Home />} />
       <Route path='/About' element={<About />} />
       <Route path='/Services' element={<Services />} />
       <Route path='/Contact' element={<Contact />} />
       </Routes>
+      <Analytics />
     </div>
   )
 }
