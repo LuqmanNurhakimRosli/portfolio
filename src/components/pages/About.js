ca886ae60607ba3d371e9a38986494697eca1c43
Luqman Nurhakim <luqmanrosli907@gmail.com>
2024-12-20 16:07:26 +0800

    try about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,11 +3,20 @@ import Navbar from '../Navbar'
 import Footer from '../Footer'
 
 function About() {
+    const [ isImportant, setIsImportant] = React.useState("Yes")
+
+    function clickButton() {
+        setIsImportant("No")
+    }
+
   return (
     <section>
         <Navbar />
         <div>
-            <h2>Contact</h2>
+            <div onClick={clickButton} className='bg-slate-500 w-4 container mx-auto p-28 flex justify-center'>
+            <h2>{isImportant}</h2>
+
+            </div>            
         </div>
         <Footer />
     </section>
