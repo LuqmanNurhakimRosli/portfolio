3303fbdedfbbaf8f55eea75b0ca93c46743fe532
Luqman Nurhakim <luqmanrosli907@gmail.com>
2024-12-24 10:40:36 +0800

    tambah

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,6 +3,22 @@ import Navbar from '../Navbar'
 import Footer from '../Footer'
 
 function About() {
+  const [contact, setContact] = useState({
+    name: "Luqman",
+    age: "20",
+    status: "Student",
+    isFavourite: true
+  })
+
+  let starIcon = contact.isFavourite ? 'star-filled.jpg' : 'star-empty.png'
+
+  function onStar() {
+    setContact(prevContact => ({
+      ...prevContact,
+      isFavourite: !prevContact.isFavourite
+    }))
+  }
+
   const [kira, setKira] = useState(0)
   const [addThings, setAddThings] = useState(["1 biri-biri", "2 biri-biri"])
 
@@ -31,8 +47,20 @@ function About() {
   return (
     <section className='min-h-screen flex flex-col bg-gray-100'>
       <Navbar />
+      <article className='bg-white p-8 rounded-lg shadow-md mx-auto w-80 my-8 '>
+      <h1 className='text-4xl font-bold text-center mb-8'>About Me</h1>
+        <div className='flex justify-center mb-4'>
+          <img onClick={onStar} src={`/images/${starIcon}`} alt='Star Icon' className='w-8 h-8 cursor-pointer' />
+        </div>
+        <div className='flex justify-center mb-4'>
+          <img src='/images/gambar-profile.jpg' alt='Profile' className='w-32 h-32 rounded-full shadow-md' />
+        </div>
+        <h2 className='text-2xl font-semibold text-center mb-2'>{contact.name}</h2>
+        <p className='text-center text-gray-700 mb-2'>{contact.age} years old</p>
+        <h2 className='text-center text-gray-700'>{contact.status}</h2>
+      </article>
+
       <div className='flex-grow container mx-auto p-8'>
-        <h1 className='text-4xl font-bold text-center mb-8'>About Me</h1>
         <div className='bg-white p-8 rounded-lg shadow-md'>
           <div className='mb-4'>
             <h2 className='text-2xl font-semibold mb-4'>Counter</h2>
@@ -54,9 +82,7 @@ function About() {
           </div>
         </div>
       </div>
-      <article >
-        
-      </article>
+      
       <Footer />
     </section>
   )
