2c2239f6a30a4d11b926aed92f6399e11ea0ed65
Luqman Nurhakim <luqmanrosli907@gmail.com>
2024-12-28 15:53:00 +0800

    wow emial

--- src/components/pages/Form.js
+++ src/components/pages/Form.js
@@ -3,7 +3,8 @@ import Navbar from '../Navbar'
 import Footer from '../Footer'
 
 function Form() {
-  const [dataForm, setDataForm] = useState ({ firstName: '', lastName:''})
+  const [dataForm, setDataForm] = useState 
+  ({ firstName: '', lastName:'',email:''})
 
   console.log(dataForm)
 
@@ -46,6 +47,19 @@ function Form() {
               type='text' 
               placeholder='Enter your last name' 
               name='lastName'
+              onChange={handleChange}
+              className='w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+            />
+          </div>
+          
+          <div className='mb-4'>
+            <label className='block text-gray-700 text-sm font-bold mb-2'>
+              Email
+            </label>
+            <input 
+              type='email' 
+              placeholder='Enter your email' 
+              name='email'
               onChange={handleChange}
               className='w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
             />
