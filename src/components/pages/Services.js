d7a63b042b4deef8f80a3f20d34c487094567f8c
Luqman Nurhakim <luqmanrosli907@gmail.com>
2024-12-28 14:48:02 +0800

    plus

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
 import Navbar from '../Navbar'
 import Footer from '../Footer'
 import memesData from '../Data/memesData'
@@ -84,6 +85,12 @@ function Services() {
           
 
         </form>
+      </div>
+
+      <div>
+        <button>
+          <Link to='/Form'>Form Page</Link>
+        </button>
       </div>
 
       <Footer />
