63b26b32167aa27135cf95af54976c5ee026581f
Luqman Nurhakim <luqmanrosli907@gmail.com>
2024-12-28 15:29:17 +0800

    wow

--- src/components/pages/Form.js
+++ src/components/pages/Form.js
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Navbar from '../Navbar'
 import Footer from '../Footer'
 
 function Form() {
+      const [firstName, setFirstName] = useState('')
+      const [lastName, setLastName] = useState('')
+
+      console.log(firstName,lastName)
+
+      function handleChangeFirst(event) {
+        setFirstName(event.target.value)}
+      
+      function handleChangeLast(event) {
+        setLastName(event.target.value)}
+      
+
+
   return (
     <div className='min-h-screen flex flex-col bg-gray-100'>
       <Navbar />
       <div className='container mx-auto p-8'>
         <h1 className='text-2xl font-bold mb-4'>Form Page</h1>
-        {/* Add your form content here */}
+
+        <form>
+
+          <input 
+            type='text' 
+            placeholder='First Name' 
+            onChange={handleChangeFirst}
+
+            />
+            <input 
+            type='text' 
+            placeholder='Last Name' 
+            onChange={handleChangeLast}
+
+            />
+
+        </form>
       </div>
       <Footer />
     </div>
