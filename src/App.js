75090d46ecd360a9ad8532d1b65e151284bebd1a
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-01-18 21:54:38 +0800

    camtu

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter ,Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter ,Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home.js';
 import Form from './components/pages/Form.js';
@@ -8,23 +8,29 @@ import Projects from './components/pages/Projects.js';
 import MemePage from './components/pages/MemePage.js';
 import Contact from './components/pages/Contact.js';
 import { Analytics } from "@vercel/analytics/react";
+import Navbar from './components/Navbar.js';
+import Footer from './components/Footer.js';
 
 
 
 export default function App () {
   
   return (
-    <div className='app'>
-      <Routes>
-      <Route path='*' element={<h1>404 - Page Not Found</h1>} />
-      <Route path='/' element={<Home />} />
-      <Route path='/About' element={<About />} />
-      <Route path='/Projects' element={<Projects />} />
-      <Route path='/Contact' element={<Contact />} />
-      <Route path='/Form' element={<Form />} />
-      <Route path='/MemePage' element={<MemePage />} />
-      </Routes>
-      <Analytics  />
-    </div>
+    <BrowserRouter>
+      <div className='app'>
+        <Navbar />
+        <Routes>
+        <Route path='*' element={<h1>404 - Page Not Found</h1>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/About' element={<About />} />
+        <Route path='/Projects' element={<Projects />} />
+        <Route path='/Contact' element={<Contact />} />
+        <Route path='/Form' element={<Form />} />
+        <Route path='/MemePage' element={<MemePage />} />
+        </Routes>
+        <Analytics  />
+        <Footer />
+      </div>
+    </BrowserRouter>
   )
 }
