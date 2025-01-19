2c5f5f2e519b50b4bb2167b7e2458ac1bee376e6
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-01-19 16:51:11 +0800

    tambah

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import { Analytics } from "@vercel/analytics/react";
-import AppRoutes from './AppRoutes'; 
-import Layout from './components/layout/Layout';
-import LoadingSpinner from './components/common/LoadingSpinner';
-import ErrorBoundary from './components/common/ErrorBoundary';
+import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Projects from '../pages/Projects';
+import Contact from '../pages/Contact';
+import MemePage from '../pages/MemePage';
 import './App.css';
 
-export default function App() {
+const App = () => {
   return (
-    <ErrorBoundary>
-      <BrowserRouter>
-        <Layout>
-          <Suspense fallback={<LoadingSpinner />}>
-            <AppRoutes />
-            <Analytics />
-          </Suspense>
-        </Layout>
-      </BrowserRouter>
-    </ErrorBoundary>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/memepage" element={<MemePage />} />
+      <Analytics />
+
+    </Routes>
   );
-}
+};
+
+export default App;
