bd6afb0ca55c8331af2e8892edb4e92be6ce0bf3
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-01-19 17:01:06 +0800

    berjaya

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import { Analytics } from "@vercel/analytics/react";
-import './App.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home';
-import About from '../pages/About';
-import Projects from '../pages/Projects';
-import Contact from '../pages/Contact';
-import MemePage from '../pages/MemePage';
-import './App.css';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Projects from './components/pages/Projects';
+import Contact from './components/pages/Contact';
+import MemePage from './components/pages/MemePage';
 
-const App = () => {
+function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
@@ -17,10 +14,8 @@ const App = () => {
       <Route path="/projects" element={<Projects />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/memepage" element={<MemePage />} />
-      <Analytics />
-
     </Routes>
   );
-};
+}
 
 export default App;
